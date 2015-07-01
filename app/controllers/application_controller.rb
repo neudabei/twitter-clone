151f4c800109fb11a59440be20d4359bf1a06f86
neudabei <rschultzejena@gmail.com>
2015-07-01 12:22:08 +0100

    added authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    # if @current_user exists, dont' run the part of the code after ||=
+    # It's called "Memoization"
+  end
+
+  def logged_in?
+    !!current_user
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+
+  root to: 'users#index'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+
   resources :users, only: [:new, :create]
   resources :statuses, only: [:new, :create]
   
   get "/:username", to: "users#show", as: 'user'
+
 end
